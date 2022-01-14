7cbe788fdde36df29dfbc862ae6d8fde35957ed4
Nikoly Cover Pereira <nikoly_cover@outlook.com>
2022-01-14 17:41:34 -0300

    adding icons folder

---
+++ weather.js
@@ -0,0 +1,11 @@
+const api = {
+    key: "7c7ffb30d0e820a9261d2a43dc258e93",
+    base: "https://api.openweathermap.org/data/2.5/",
+    lang: "pt br",
+    units: "metric"
+}
+
+let obj = {
+    name: "nikoly",
+    idade: 17
+}
