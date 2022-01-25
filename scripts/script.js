b8b6ef4f99e23cda74012b984478b407664a7755
Nikoly Cover Pereira <nikoly_cover@outlook.com>
2022-01-25 15:52:50 -0300

    css details

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,19 @@
 const containerWidth = document.querySelector('.weather-forecast').offsetWidth;
 
+const cardMargin = document.querySelector('.forecast-card').style.marginRight;
+
 const track = document.querySelector('.track')
 
 const prev = document.querySelector('.prev')
 const next = document.querySelector('.next')
 
 next.addEventListener('click', () => {
-    track.style.transform = `translateX(-100%)`;
+    track.style.transform = `translateX(-104%)`;
+    /*track.style.transform = `translateX(-${containerWidth + cardMargin}px)`;*/
 })
 
 prev.addEventListener('click', () => {
     track.style.transform = `translateX(0)`;
 })
 
-console.log(containerWidth)
+console.log(cardMargin)
