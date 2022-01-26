e77173a5064e873ba2f52754fe588fddb40acbc8
Nikoly Cover Pereira <nikoly_cover@outlook.com>
2022-01-25 21:48:30 -0300

    adding search function

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,4 @@
-const containerWidth = document.querySelector('.weather-forecast').offsetWidth;
-
-const cardMargin = document.querySelector('.forecast-card').style.marginRight;
-
 const track = document.querySelector('.track')
-
 const prev = document.querySelector('.prev')
 const next = document.querySelector('.next')
 
@@ -14,4 +9,16 @@ next.addEventListener('click', () => {
 
 prev.addEventListener('click', () => {
     track.style.transform = `translateX(0)`;
+})
+
+const searchButton = document.querySelector('.search-city-button')
+
+searchButton.addEventListener("click", function(e) {
+    e.preventDefault()
+
+    const searchCityInput = document.querySelector('.search-city-input')
+    
+    var city = searchCityInput.value
+
+    insertData(request(city))
 })
--- scripts/weather.js
+++ scripts/weather.js
@@ -5,43 +5,55 @@ const api = {
     units: "metric"
 }
 
-//current weather
-const cityName = document.querySelector('#city-name')
-const date = document.querySelector('#date')
-const temp = document.querySelector('#current-temp')
-const icon = document.querySelector('#weather-icon')
-const descr = document.querySelector('#weather-descr')
-const feelsLike = document.querySelector('#feels-like')
-const maxTemp = document.querySelector('#max-temp')
-const minTemp = document.querySelector('#min-temp')
-const humidity = document.querySelector('#humidity')
-const windy = document.querySelector('#wind')
-
 function insertData(data) {
+    const cityName = document.querySelector('#city-name')
     cityName.textContent = data.name + ' - ' + data.sys.country
 
+    const date = document.querySelector('#date')
     let dt = new Date();
     let day = String(dt.getDate()).padStart(2, '0')
     let month = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"][dt.getMonth()]
     let year = dt.getFullYear()
-
     date.textContent = day + ' de ' + month + ' de ' + year
 
+    const temp = document.querySelector('#current-temp')
     temp.textContent = Math.trunc(data.main.temp)
 
+    const icon = document.querySelector('#weather-icon')
     icon.src = `media/weather-icons/${data.weather[0].icon}.png`
 
+    const descr = document.querySelector('#weather-descr')
     descr.textContent = data.weather[0].description
 
+    const feelsLike = document.querySelector('#feels-like')
     feelsLike.textContent = Math.trunc(data.main.feels_like)
+
+    const maxTemp = document.querySelector('#max-temp')
     maxTemp.textContent = Math.trunc(data.main.temp_max)
+
+    const minTemp = document.querySelector('#min-temp')
     minTemp.textContent = Math.trunc(data.main.temp_min)
+
+    const humidity = document.querySelector('#humidity')
     humidity.textContent = data.main.humidity
+
+    const windy = document.querySelector('#wind')
     windy.textContent = Math.trunc(data.wind.speed * 3.708) //meter per second to km per hour
 }
 
-fetch(`${api.base}weather?q=${city = 'Foz Do Iguaçu'}&lang=${api.lang}&units=${api.units}&appid=${api.key}`)
+function request(cityName) {
+    fetch(`${api.base}weather?q=${cityName}&lang=${api.lang}&units=${api.units}&appid=${api.key}`)
     .then(response => { response.json()
         .then( data => insertData(data))
     })
     .catch(e => console.log(`Error: ${e.message}`))
+
+    /*fetch(`${api.base}forecast?q=${cityName}&lang=${api.lang}&units=${api.units}&appid=${api.key}`)
+    .then(response => { response.json()
+        .then( data => console.log(data))
+    })
+    .catch(e => console.log(`Error: ${e.message}`))*/
+
+}
+
+request('Brasilia')
