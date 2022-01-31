c11093127e4e176ed068a5f2b8932438b2b74b17
Nikoly Cover <nikolycoverpereira@gmail.com>
2022-01-31 14:43:36 -0300

    feature: search city when enter is pressed

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
 const track = document.querySelector('.track')
 const prev = document.querySelector('.prev')
 const next = document.querySelector('.next')
+const searchInput = document.querySelector('.search-city-input')
+const searchButton = document.querySelector('.search-city-button')
+
+const searchCity = city => {
+    insertData(request(city))
+}
 
 next.addEventListener('click', () => {
     track.style.transform = `translateX(-104%)`;
@@ -11,14 +17,19 @@ prev.addEventListener('click', () => {
     track.style.transform = `translateX(0)`;
 })
 
-const searchButton = document.querySelector('.search-city-button')
 
-searchButton.addEventListener("click", function(e) {
-    e.preventDefault()
+searchButton.addEventListener("click", event => {
+    event.preventDefault()
 
-    const searchCityInput = document.querySelector('.search-city-input')
-    
-    var city = searchCityInput.value
+    searchCity(searchInput.value)
+})
 
-    insertData(request(city))
+searchInput.addEventListener('keyup', event => {
+    if(event.keyCode == 13){
+        searchCity(searchInput.value)
+    }
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    searchInput.value = ""
 })
